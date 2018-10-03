09196d02240ff3bc64ae6fff45496d378c9abe9f
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-10-02 22:01:29 -0500

    inital LogIn page Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LogIn from './components/LogIn'
+import LogIn from './LogIn'
 class App extends Component {
   render() {
     return (
