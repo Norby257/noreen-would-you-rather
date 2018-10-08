848d231d219c7efba7e9ace18fcf19c45d379806
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-10-07 20:57:33 -0500

    initial newQuestion

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import LogIn from './LogIn'
 import Question from './Question'
+import NewQuestion from './NewQuestion'
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -16,6 +18,7 @@ class App extends Component {
         </p>
         <LogIn />
         <Question />
+        <NewQuestion />
       </div>
     );
   }
--- src/NewQuestion.js
+++ src/NewQuestion.js
@@ -1,2 +1,42 @@
 import React, { Component } from "react"
- 
+
+ class NewQuestion extends Component {
+     state = {
+         question: ''
+     }
+
+     //  methods here ; also, may have to include more info in the state
+
+     // 1) capture option one from user 
+     // 2) capture option two from user 
+     // 3) allow submission 
+
+     // displaying component to the screen 
+
+     render() {
+         // other vars and dispatch go here 
+
+         // actual content on screen
+        return (
+            <div>
+            <h3 className="center">Submit a dilemma!  </h3>
+            <form className="new-tweet" onSubmit={this.handleSubmit}>
+           <textarea 
+                placeholder="Choice 1 "
+                // value={text}
+                // onChange={this.handleChange}
+             
+                />
+             
+
+                <button className='btn'>
+               
+                SEND IN YOUR DILEMMA
+                 </button>
+            </form>
+        </div>
+        )
+     }
+ }
+
+ export default NewQuestion;
